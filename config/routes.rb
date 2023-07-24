0ed6cc052e438b9ed06f5605d0623814fdce0fab
Lucas Rocha <lr359508@gmail.com>
2023-07-24 19:00:39 -0300

    Criação do model article.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/articles", to: "articles#index"
+  root "articles#index"
+
+  resources :articles
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
